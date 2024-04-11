98ba90f11dd9debff08d323f3cf22c0f667f3a64
iwanharyatno <iwnharry61@gmail.com>
2024-04-11 16:27:54 +0700

    calculator: refactoring pt 2

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -1,28 +1,27 @@
 const layar = document.querySelector(".screen");
+const papanKalkulator = document.querySelector(".calculator-body");
 
-document
-  .querySelector(".calculator-body")
-  .addEventListener("click", function (event) {
-    if (event.target.tagName !== "INPUT") return;
+papanKalkulator.addEventListener("click", function (event) {
+  if (event.target.tagName !== "INPUT") return;
 
-    if (event.target.classList.contains("action-clear")) {
-      layar.innerText = "";
-      return;
+  if (event.target.classList.contains("action-clear")) {
+    layar.innerText = "";
+    return;
+  }
+
+  if (event.target.classList.contains("action-backspace")) {
+    layar.innerText = layar.innerText.slice(0, -1);
+    return;
+  }
+
+  if (event.target.classList.contains("action-evaluate")) {
+    try {
+      layar.innerText = eval(layar.innerText);
+    } catch (err) {
+      layar.innerText = "Error";
     }
-    
-    if (event.target.classList.contains("action-backspace")) {
-      layar.innerText = layar.innerText.slice(0, -1);
-      return;
-    }
-    
-    if (event.target.classList.contains("action-evaluate")) {
-      try {
-        layar.innerText = eval(layar.innerText);
-      } catch (err) {
-        layar.innerText = "Error";
-      }
-      return;
-    }
-    
-    layar.innerText += event.target.value;
-  });
+    return;
+  }
+
+  layar.innerText += event.target.value;
+});
