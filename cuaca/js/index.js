c6089a9f22553ca86bc213e68a8b898cde650c2c
Didi Prasetyo <didiprasetyo360@gmail.com>
2024-05-17 15:41:47 +0700

    feat: search weather by city name

--- cuaca/js/index.js
+++ cuaca/js/index.js
@@ -1,27 +1,59 @@
-const mainWeather = document.getElementById('cuaca-utama-wind');
+const lokasi = document.getElementById('lokasi');
+const kota = document.getElementById('kota');
 
+const cuacaUtamaTemp = document.getElementById('cuaca-utama-temp');
+const cuacaUtamaWind = document.getElementById('cuaca-utama-wind');
+const cuacaUtamahumidity = document.getElementById('cuaca-utama-humidity');
 
-async function getWeather() {
-    const url = 'https://api.openweathermap.org/data/2.5/forecast?q=purwokerto&appid=d8b21c07ae373d57cedb303d944e393e';
+const cuaca1Temp = document.getElementById('cuaca-1-temp');
+const cuaca1Wind = document.getElementById('cuaca-1-wind');
+const cuaca1humidity = document.getElementById('cuaca-1-humidity');
+
+const cuaca2Temp = document.getElementById('cuaca-2-temp');
+const cuaca2Wind = document.getElementById('cuaca-2-wind');
+const cuaca2humidity = document.getElementById('cuaca-2-humidity');
+
+const cuaca3Temp = document.getElementById('cuaca-3-temp');
+const cuaca3Wind = document.getElementById('cuaca-3-wind');
+const cuaca3humidity = document.getElementById('cuaca-3-humidity');
+
+async function getWeather(city = 'purwokerto') {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=d8b21c07ae373d57cedb303d944e393e&units=metric`;
 
     return await fetch(url, {
         method: 'GET',
     })
     .then(response => {
-        // Check if the response is successful
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
-        // Parse the JSON from the response
         return response.json();
     }).then((result) => {
-        mainWeather.innerText = "hello";
+        cuacaUtamaTemp.innerText = result.list[0].main.temp + "°C"
+        cuacaUtamaWind.innerText = result.list[0].wind.speed + " km/hr";
+        cuacaUtamahumidity.innerText = result.list[0].main.humidity + "%";
+
+        cuaca1Temp.innerText = result.list[1].main.temp + "°C"
+        cuaca1Wind.innerText = result.list[1].wind.speed + " km/hr";
+        cuaca1humidity.innerText = result.list[1].main.humidity + "%";
+
+        cuaca2Temp.innerText = result.list[1].main.temp + "°C"
+        cuaca2Wind.innerText = result.list[1].wind.speed + " km/hr";
+        cuaca2humidity.innerText = result.list[1].main.humidity + "%";
+
+        cuaca3Temp.innerText = result.list[3].main.temp + "°C"
+        cuaca3Wind.innerText = result.list[3].wind.speed + " km/hr";
+        cuaca3humidity.innerText = result.list[3].main.humidity + "%";
+
         console.log(result);
     });    
 }
 
 getWeather()
 
-// const result = getWeather();
-
-// console.log(result);
+function getByCity() {
+    const lokasiValue = lokasi.value;
+    kota.innerText = lokasiValue;
+    
+    getWeather(lokasiValue)
+}
